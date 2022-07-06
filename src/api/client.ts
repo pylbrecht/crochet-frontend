a2eb6f7f7799624896b25afe62b62848ada64f6d
Philipp Albrecht <philipp.albrecht@momox.biz>
2022-07-06 20:57:41 +0200

    Add api client mock
    I just need something to return mock data, which I can replace later
    with a proper backend.

---
+++ src/api/client.ts
@@ -0,0 +1,37 @@
+const projects = [
+  {
+    id: 0,
+    title: "Seelenwärmer",
+    description: "Lorem ipsum",
+    needle: 1,
+  },
+  {
+    id: 1,
+    title: "Schal",
+    description: "Lorem ipsum",
+    needle: 2,
+  },
+  {
+    id: 2,
+    title: "Schalütze",
+    description: "Lorem ipsum",
+    needle: 3,
+  },
+];
+
+type Project = {
+  id: number;
+  title: string;
+  description: string;
+  needle: number;
+};
+
+function getProject(id: number): Project | undefined {
+  return projects.find(p => p.id === id);
+}
+
+function getProjects(): Project[] {
+  return projects;
+}
+
+export { getProject, getProjects };
