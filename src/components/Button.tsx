c553585d4dbdc5d5916f279c5c9ce7d1baa5d61b
Philipp Albrecht <palbrecht@mailbox.org>
2022-11-12 14:17:59 +0100

    Add className to ButtonProps
    This way I can pass apply addtional styles.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,14 +2,17 @@ import React from "react";
 
 type ButtonProps = {
   children: React.ReactNode;
+  className?: string;
   onClick: (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-const Button = ({ children, onClick }: ButtonProps) => {
+const Button = ({ children, onClick, className }: ButtonProps) => {
   return (
     <button
       onClick={onClick}
-      className="py-2 px-4 shadow outline-0 rounded-full border-0 bg-neutral-900 text-neutral-50 font-bold hover:bg-neutral-700"
+      className={`py-2 px-4 shadow outline-0 rounded-full border-0 bg-neutral-900 text-neutral-50 font-bold hover:bg-neutral-700 ${
+        className ?? ""
+      }`}
     >
       {children}
     </button>
