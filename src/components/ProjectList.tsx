f268e7f24b803037e11c81e92a1969a73491e3da
Philipp Albrecht <palbrecht@mailbox.org>
2022-11-12 14:56:46 +0100

    Restyle ProjectList
    I adopted the styling I applied to the NewProjectForm component, which
    is currently developed in another branch.

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -8,16 +8,16 @@ type ProjectListProps = {
 
 export const ProjectList = ({ projects, onClickHandler }: ProjectListProps) => {
   return (
-    <ul>
+    <ul className="bg-neutral-50 rounded-lg drop-shadow">
       {projects.map((project) => (
         <li
-          className="first:border-t-2 last:border-b-2 border-b py-1 hover:cursor-pointer"
+          className="border-b p-2 hover:cursor-pointer"
           key={project.id}
           onClick={onClickHandler}
         >
           <div className="flex">
             <div className="w-16 h-16 inline-block bg-neutral-900 m-1"></div>
-            <div className="flex flex-col justify-around ml-1 mb-1">
+            <div className="flex flex-col justify-around ml-3 mb-1">
               <h2 className="inline text-xl font-bold">{project.name}</h2>
               <p>Hook size: {project.hookSize}</p>
             </div>
